f692fba43b7530c8fa057dd5794f6108bdbf5896
MaciejKlepacki <maciek.klepacki@icloud.com>
2024-06-04 18:47:26 +0200

    Added welcome

--- script.js
+++ script.js
@@ -76,6 +76,11 @@ cells.forEach(cell => {
 })
 const winning = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
+const newFeature = function() {
+    console.log('welcome');
+}
+
+newFeature();
 const checkTheWin = function () {
     for (const winCombo of winning) {
         if (winCombo.every((element) => whereX.includes(element))) {
